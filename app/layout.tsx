2e7c0f5e3bb0c6b7918ceddb37669a4b829647b2
sanjeevnode <me.sanjeevks@gmail.com>
2023-12-17 00:07:30 +0530

    starter

---
+++ app/layout.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from "next";
+import { Poppins } from "next/font/google";
+import "./globals.css";
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+  variable: "--font-poppins",
+});
+
+export const metadata: Metadata = {
+  title: "Evently - Event Management System",
+  description: "Evently is an event management system built with Next.js",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={poppins.variable}>{children}</body>
+    </html>
+  );
+}
